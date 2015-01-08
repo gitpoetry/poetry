42d3ca21fe3eb0144b149c363dac314b8ab03e7a
Andrew Twopence <gitpoetry@gmail.com>
2015-01-08 11:11:24 -0700

    started lion_king

---
+++ lion_king.rb
@@ -0,0 +1,32 @@
+while we made out in my parents' kitchen
+	there was a moment when my brain
+	superimposed her face onto yours and
+	i couldn't say a word, knowing it
+	would only be her name
+
+i had a dream last night where a blonde girl
+in a summer dress
+begged me to take her back and i still
+dont know if it was you or her
+
+part of me is scared of the mental association,
+the similarites that seem so obvious now
+
+but i am actually relieved
+
+for the hope that one day i can:
+	walk through my parents' kitchen
+	bake sugar cookies
+	run down hull's gulch
+	listen to my favorite band
+	look at my phone
+	watch the sunset
+
+the way i can:
+	walk by the river
+	cook quesadillas
+	run by centennial
+	listen to my old favorite band
+	answer my phone
+	watch the lion king
+	 
